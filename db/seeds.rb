46bd198a0edd7818e2ca9c0d223fe11f76fefa18
Ben Prat <bvprat@gmail.com>
2021-02-23 18:33:26 -0700

    Create first product

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+product1 = Product.new(name: "Model Car", price: 15, image_url: "https://www.randomlists.com/img/things/model_car.webp", description: "Model Car Set")
+
+product1.save
+p product1
